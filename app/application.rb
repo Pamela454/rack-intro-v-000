c956228bde7f21f15e4f99f306bb8ff83c438e1c
Pamela Frieze <pfrieze@gmail.com>
2018-06-15 15:41:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World. Hello my name is Pamela."
+    resp.write "Hello, World. Hello, my name is Pamela."
     resp.finish
   end
 
